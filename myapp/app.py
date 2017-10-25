9c7423cf12b50267d8c09b4390f63f0669c1a90a
chrisossimo <christoph_bossmann@arcor.de>
2017-10-25 14:46:07 +0000

    New files added

--- myapp/app.py
+++ myapp/app.py
@@ -1,64 +1,52 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template 
 from flask_mysqldb import MySQL
-from flask_prometheus import monitor 
 mysql = MySQL()
 app = Flask(__name__)
 # My SQL Instance configurations 
 # Change the HOST IP and Password to match your instance configurations
- 
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'P@ssw0rd'
-app.config['MYSQL_DB'] = 'studentbook'
-app.config['MYSQL_HOST'] = '35.195.195.114'
+app.config['MYSQL_PASSWORD'] = 'mysqldb'
+app.config['MYSQL_DB'] = 'todolist'
+app.config['MYSQL_HOST'] = '35.189.126.125'
 mysql.init_app(app)
 
-@app.route('/')
-@app.route('/<name>')
-def statichtml(name=None):
-    return render_template('index.html', name=name)
-
-# The first route to access the webservice from http://35.190.217.3:5000/ 
+# The first route to access the webservice from http://external-ip:5000/ 
 #@pp.route("/add") this will create a new endpoints that can be accessed using http://external-ip:5000/add
-@app.route("/list")
+@app.route("/")
+def index(): 
+    return render_template("index.html")
+
+@app.route("/list") 
 def hello(): # Name of the method
     cur = mysql.connection.cursor() #create a connection to the SQL instance
-    cur.execute('''SELECT * FROM students''') # execute an SQL statment
+    cur.execute('''SELECT * FROM todo''') # execute an SQL statment
     rv = cur.fetchall() #Retreive all rows returend by the SQL statment
-    return render_template('index.html', name=str(rv))     #Return the data in a string format
+    return str(rv)      #Return the data in a string format
 
-@app.route("/add/<name>/<email>")
-def add(name=None, email=None):
-    cur= mysql.connection.cursor()
-    insert_stmt = (
-                 "INSERT INTO students (studentName, email) "
-                 "VALUES (%s, %s)")
-    data=(name,email)
-    cur.execute(insert_stmt, data)
+@app.route("/add/<todotext>")
+def insert(todotext):
+    cur = mysql.connection.cursor() #create a connection to the SQL instance
+    cur.execute("INSERT INTO todo (text) values (%s)",(todotext,)) # execute an SQL statment
     mysql.connection.commit()
-    return render_template('index.html', name="New Record is added to the database")  
+    return 'ToDo added!' #Return the data in a string format
 
-@app.route("/update/<name>/<email>")
-def update(name=None, email=None):
-    cur=mysql.connection.cursor()
-    update_stmt = (
-        "UPDATE students SET studentName = %s " 
-        "WHERE email = %s")
-    data=(name,email)
-    cur.execute(update_stmt, data)
+@app.route("/update/<id>/<todotext>")
+def update(id, todotext):
+    cur = mysql.connection.cursor() #create a connection to the SQL instance
+    cur.execute("UPDATE todo SET text=%s WHERE todoID=%s",(todotext, id)) # execute an SQL$
     mysql.connection.commit()
-    return render_template('index.html', name="User recored was updated")      #Return the data in a string format
+    return 'Todo updated' #Return the data in a string format
 
-@app.route("/delete/<name>")
-def delete(name=None):
-    cur=mysql.connection.cursor()
-    delstatmt = "DELETE FROM students WHERE studentName = ' {} ' ".format(name)
-    print(delstatmt)                
-   
-    cur.execute(delstatmt)
+@app.route("/delete/<id>")
+def delete(id):
+    cur = mysql.connection.cursor() #create a connection to the SQL instance
+    cur.execute("DELETE FROM todo WHERE todoID=%s",(id)) # execute an SQ$
     mysql.connection.commit()
-    return render_template('index.html', name="User recored was deleted")      #Return the data in a string format
+    return 'Delete finished' #Return the data in a string format
+
+@app.route("/mytemplate")
+def mytemplate():
+    return render_template("mytemplate.html", name='Mr X', country='Germany')
 
 if __name__ == "__main__":
-        monitor(app, port=8000)
-        app.run(host='0.0.0.0', port='5000')
+        app.run(host='0.0.0.0', port='5000') #Run the flask app at port 5000
